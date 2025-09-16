ce459187869cbdd9917224a3af9e838c8abace3a
Christian David Lancheros Martínez <109816345+PsicoChris@users.noreply.github.com>
2025-09-16 17:13:12 +0200

    bugs

--- vortex/settings.py
+++ vortex/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'principal/static'),
+    os.path.join(BASE_DIR, 'principal/static/principal'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
